0106d89fe3ee49859df43ee3a0b3ec5061f6ed77
ViharevN <swix_off@mail.ru>
2023-02-03 01:39:27 +0300

    commit

---
+++ src/main/java/me/viharev/postgreshw/PostgresHwApplication.java
@@ -0,0 +1,13 @@
+package me.viharev.postgreshw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/viharev/postgreshw/PostgresHwApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.postgreshw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
